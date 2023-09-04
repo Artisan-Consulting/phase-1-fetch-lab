8f559e2d6d4449a6ad3e78794ed5c0d462683d98
William Kwan <william.kwan@artisanconsulting.nz>
2023-09-04 22:34:50 +1000

    Added fetch

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+    //   // To pass the tests, don't forget to return your fetch!
+    //   const response = await fetch("https://anapioficeandfire.com/api/books")
+    //     const books = await response.json()
+    //     console.log (books)
+
+   const books = fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    //.then((json) => console.log(json))
+    return books
+    .then((renderBooks))
+  } ;
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -14,4 +22,6 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  console.log ("fetchBooks function call");
 });
+
